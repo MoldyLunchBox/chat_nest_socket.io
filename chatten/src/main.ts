8305ae3e9dde514f05c2d7dfb7a92986508a6938
Abderrahmane Mya <amya@e1r5p1.1337.ma>
2023-03-27 21:14:49 +0000

    simple chat app to demonstrate the use of socketss

--- chatten/src/main.ts
+++ chatten/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { env } from 'process';
 import { AppModule } from './app.module';
+import { IoAdapter } from '@nestjs/platform-socket.io';
+import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useWebSocketAdapter(new WsAdapter(app));
   await app.listen(parseInt(process.env.PORT) || 3000);
 }
 bootstrap();
